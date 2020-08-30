8c558f855947c5dad4f8b5b4df77498cbe3199e9
Brayan Forero <=brayanroswll2802@gmail.com>
2020-08-30 08:51:19 -0400

    Agrega funcionalidad de registro y listar personal

--- src/routes/workers.js
+++ src/routes/workers.js
@@ -1,2 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../database/connection");
+
+router.get("/", async (req, res) => {
+  const workers = await db.query(
+    "SELECT id_worker AS id,worker_document AS doc , name, last_name, id_position AS position, state FROM workers"
+  );
+  res.render("workers/listworker", { workers });
+});
+router.get("/add", async (req, res) => {
+  const positions = await db.query("SELECT id_position, name FROM positions");
+  res.render("workers/newworker", {
+    positions,
+    success: req.flash("success")[0],
+  });
+});
+
+router.post("/add", async (req, res) => {
+  const { document, name, lastname, position } = req.body;
+
+  const result = await db.query(
+    "INSERT INTO workers (worker_document, name, last_name, id_position) VALUES (?,?,?,?)",
+    [document, name, lastname, position]
+  );
+  req.flash("success", "Regitro completado");
+  res.redirect("/workers/add");
+});
+module.exports = router;
