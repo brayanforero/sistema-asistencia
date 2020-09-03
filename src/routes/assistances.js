85c6a98949eb5bce1f72dd7a2c5f44d2d3400bab
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-03 15:15:52 -0400

    Lista y agrega saldidas

--- src/routes/assistances.js
+++ src/routes/assistances.js
@@ -24,7 +24,6 @@ router.get("/entrance/add", async (req, res) => {
 router.post("/entrance/add", async (req, res) => {
   let { journal, worker, assist, observation } = req.body;
 
-  console.log(req.body);
   try {
     const rows = await db.query(
       "INSERT INTO entrances (id_journal, id_worker, is_assist, observation, time_entrance) VALUES(?,?,?,?,CURRENT_TIME)",
@@ -33,9 +32,46 @@ router.post("/entrance/add", async (req, res) => {
     req.flash("success", "Operacion completa con Exito");
     res.redirect("/assistances/entrance/add");
   } catch (error) {
-    console.log(error);
     req.flash("falied", "Operacion fallida");
     res.redirect("/assistances/entrance/add");
   }
 });
+
+// render de vista lista de salidas
+router.get("/exit", async (req, res) => {
+  const departures = await db.query(
+    "SELECT de.id_departure AS id,wrk.worker_document AS doc,concat(wrk.name, ' ', wrk.last_name) AS person,wrk.state,de.is_assist,time_format(de.time_entrance, '%h:%i %p') AS time,de.observation AS ob,date_format(jor.created_at,'%d/%m/%Y') AS journal FROM departures AS de INNER JOIN workers AS wrk	ON wrk.id_worker = de.id_worker INNER JOIN journal AS jor ON jor.id_journal = de.id_journal"
+  );
+  res.render("departures/exits.hbs", { departures });
+});
+
+// render de vista para agregar una salida
+
+router.get("/exit/add", async (req, res) => {
+  const journals = await db.query(
+    "SELECT id_journal AS id, date_format(created_at,'%d-%m-%Y') AS fecha FROM journal;"
+  );
+  const workers = await db.query(
+    "SELECT id_worker AS id, CONCAT(name,' ',last_name ) AS fullname FROM workers;"
+  );
+  res.render("departures/newexit", { journals, workers });
+});
+
+// proceso para agregar una salida
+router.post("/exit/add", async (req, res) => {
+  let { journal, worker, assist, observation } = req.body;
+
+  try {
+    const rows = await db.query(
+      "INSERT INTO departures (id_journal, id_worker, is_assist, observation, time_entrance) VALUES(?,?,?,?,CURRENT_TIME)",
+      [journal, worker, assist, observation]
+    );
+    req.flash("success", "Operacion completa con Exito");
+    res.redirect("/assistances/exit/add");
+  } catch (error) {
+    req.flash("falied", "Operacion fallida");
+    res.redirect("/assistances/exit/add");
+  }
+});
+
 module.exports = router;
