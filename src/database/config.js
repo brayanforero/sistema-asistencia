c7bdea4de4c837b0ea1d6cbcfe6f742b9f93f785
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-08 23:30:55 -0400

    Corrige color en login

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,9 @@
 const dataConfig = {
-  host: "localhost",
+  host: "bbzouqlqlwrdzf8a8q84-mysql.services.clever-cloud.com",
   port: 3306,
-  user: "root",
-  password: "root123*",
-  database: "colegio_asistencias",
+  user: "ubalqxvqnnjkus7w",
+  password: "OpIsjaSyjGaiTnN635Vo",
+  database: "bbzouqlqlwrdzf8a8q84",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
