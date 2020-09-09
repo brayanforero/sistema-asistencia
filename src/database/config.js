79c24c762f516a30816a5fcac1290bd45091962e
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-08 22:37:00 -0400

    Solo se trabaja con datos locales

--- src/database/config.js
+++ src/database/config.js
@@ -1,20 +1,11 @@
 const dataConfig = {
-  host: "bbzouqlqlwrdzf8a8q84-mysql.services.clever-cloud.com",
+  host: "localhost",
   port: 3306,
-  user: "ubalqxvqnnjkus7w",
-  password: "OpIsjaSyjGaiTnN635Vo",
-  database: "bbzouqlqlwrdzf8a8q84",
+  user: "root",
+  password: "root123*",
+  database: "colegio_asistencias",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
-// const dataConfig = {
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root123*",
-//   database: "colegio_asistencias",
-//   charset: "utf8_general_ci",
-//   timezone: "-0400",
-// };
 
 module.exports = dataConfig;
