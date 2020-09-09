d311e2bed79280a4231f9d710dd33326a8e2519e
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-08 23:52:03 -0400

    Agrega color azul en login

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,9 @@
 const dataConfig = {
-  host: "bbzouqlqlwrdzf8a8q84-mysql.services.clever-cloud.com",
+  host: "localhost",
   port: 3306,
-  user: "ubalqxvqnnjkus7w",
-  password: "OpIsjaSyjGaiTnN635Vo",
-  database: "bbzouqlqlwrdzf8a8q84",
+  user: "root",
+  password: "root123*",
+  database: "colegio_asistencias",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
