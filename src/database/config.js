eb6c591d4f829bfc0d02416af4811d02628aefa7
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-09 00:05:41 -0400

    Deploy

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,18 @@
+// const dataConfig = {
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root123*",
+//   database: "colegio_asistencias",
+//   charset: "utf8_general_ci",
+//   timezone: "-0400",
+// };
 const dataConfig = {
-  host: "localhost",
+  host: "bnddrwm5dused9xo53m7-mysql.services.clever-cloud.com",
   port: 3306,
-  user: "root",
-  password: "root123*",
-  database: "colegio_asistencias",
+  user: "urakzgw4atem4att",
+  password: "WM5yYILRJMKMo54ICMJ0",
+  database: "bnddrwm5dused9xo53m7",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
