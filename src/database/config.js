e42beb1371c3c14237206444064007609eeae6af
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-08 22:13:06 -0400

    make it better

--- src/database/config.js
+++ src/database/config.js
@@ -1,20 +1,20 @@
-// const dataConfig = {
-//   host: "bztppaxpqfihy03n9enk-mysql.services.clever-cloud.com",
-//   port: 3306,
-//   user: "uyzsfmyvdzphkxw0",
-//   password: "GYvHjYachCvi5Gip7ne6",
-//   database: "bztppaxpqfihy03n9enk",
-//   charset: "utf8_general_ci",
-//   timezone: "-0400",
-// };
 const dataConfig = {
-  host: "localhost",
+  host: "bbzouqlqlwrdzf8a8q84-mysql.services.clever-cloud.com",
   port: 3306,
-  user: "root",
-  password: "root123*",
-  database: "colegio_asistencias",
+  user: "ubalqxvqnnjkus7w",
+  password: "OpIsjaSyjGaiTnN635Vo",
+  database: "bbzouqlqlwrdzf8a8q84",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
+// const dataConfig = {
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root123*",
+//   database: "colegio_asistencias",
+//   charset: "utf8_general_ci",
+//   timezone: "-0400",
+// };
 
 module.exports = dataConfig;
