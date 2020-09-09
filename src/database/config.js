6468a1b7bdd7de5163e35a4302509da0979bef31
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-08 21:35:23 -0400

    Cmabios a local

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,18 @@
+// const dataConfig = {
+//   host: "bztppaxpqfihy03n9enk-mysql.services.clever-cloud.com",
+//   port: 3306,
+//   user: "uyzsfmyvdzphkxw0",
+//   password: "GYvHjYachCvi5Gip7ne6",
+//   database: "bztppaxpqfihy03n9enk",
+//   charset: "utf8_general_ci",
+//   timezone: "-0400",
+// };
 const dataConfig = {
-  host: "bztppaxpqfihy03n9enk-mysql.services.clever-cloud.com",
+  host: "localhost",
   port: 3306,
-  user: "uyzsfmyvdzphkxw0",
-  password: "GYvHjYachCvi5Gip7ne6",
-  database: "bztppaxpqfihy03n9enk",
+  user: "root",
+  password: "root123*",
+  database: "colegio_asistencias",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
