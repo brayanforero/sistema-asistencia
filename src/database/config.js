7f6ca017de3084a28a4715e759c8dc425866761d
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-10 12:51:59 -0400

    New credential

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,3 @@
-// const dataConfig = {
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root123*",
-//   database: "colegio_asistencias",
-//   charset: "utf8_general_ci",
-//   timezone: "-0400",
-// };
-
 const dataConfig = {
   host: "bgl0fmjtdzdwydozge3q-mysql.services.clever-cloud.com",
   port: 3306,
