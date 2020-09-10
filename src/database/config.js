a54c41a65723a9854066032d80f46d81d4360e2d
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-10 12:44:16 -0400

    Deploy de proteccion de rutas

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,19 @@
+// const dataConfig = {
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root123*",
+//   database: "colegio_asistencias",
+//   charset: "utf8_general_ci",
+//   timezone: "-0400",
+// };
+
 const dataConfig = {
-  host: "localhost",
+  host: "bgl0fmjtdzdwydozge3q-mysql.services.clever-cloud.com",
   port: 3306,
-  user: "root",
-  password: "root123*",
-  database: "colegio_asistencias",
+  user: "uq3d3yvjvd48qoww",
+  password: "kD9dINo1K6u4jfDelVgx",
+  database: "bgl0fmjtdzdwydozge3q",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
