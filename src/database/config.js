e39c848a0db5e525880b239f1332c2fcde23cb23
Brayan Forero <=brayanroswll2802@gmail.com>
2020-09-10 11:51:45 -0400

    Sincronizacion ramas

--- src/database/config.js
+++ src/database/config.js
@@ -1,18 +1,9 @@
-// const dataConfig = {
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root123*",
-//   database: "colegio_asistencias",
-//   charset: "utf8_general_ci",
-//   timezone: "-0400",
-// };
 const dataConfig = {
-  host: "bnddrwm5dused9xo53m7-mysql.services.clever-cloud.com",
+  host: "localhost",
   port: 3306,
-  user: "urakzgw4atem4att",
-  password: "WM5yYILRJMKMo54ICMJ0",
-  database: "bnddrwm5dused9xo53m7",
+  user: "root",
+  password: "root123*",
+  database: "colegio_asistencias",
   charset: "utf8_general_ci",
   timezone: "-0400",
 };
