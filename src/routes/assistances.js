e45a181720509b2023f97c38c07c53bf5f90be01
Brayan Forero <brayanroswll2802@gmail.com>
2020-10-07 20:16:21 -0400

    Formatea hora de salida en las asistencias

--- src/routes/assistances.js
+++ src/routes/assistances.js
@@ -6,17 +6,17 @@ const { isNotLogin } = require("../middlewares/auth");
 
 router.get("/", isNotLogin, async (req, res) => {
   const entrances = await db.query(
-    "SELECT en.id_entrance AS id,wrk.worker_document AS doc,concat(wrk.name, ' ', wrk.last_name) AS person,wrk.state,en.is_assist,time_format(en.time_entrance, '%h:%i %p') AS time_go,time_format(en.time_exit, '%h:%i %p') AS time_end,en.observation AS ob,date_format(jor.created_at,'%d/%m/%Y') AS journal FROM entrances AS en INNER JOIN workers AS wrk	ON wrk.id_worker = en.id_worker INNER JOIN journal AS jor ON jor.id_journal = en.id_journal"
+    "SELECT en.id_entrance AS id,wrk.worker_document AS doc,concat(wrk.name, ' ', wrk.last_name) AS person,wrk.state,en.is_assist,time_format(en.time_entrance, '%h:%i %p') AS time_go,valid_date_end(en.time_exit) AS time_end,en.observation AS ob,date_format(jor.created_at,'%d/%m/%Y') AS journal FROM entrances AS en INNER JOIN workers AS wrk	ON wrk.id_worker = en.id_worker INNER JOIN journal AS jor ON jor.id_journal = en.id_journal"
   );
   res.render("assistances/entrances", { entrances });
 });
 // render de la vista para agregar una entrada
 router.get("/entrada/", isNotLogin, async (req, res) => {
   const journals = await db.query(
-    "SELECT id_journal AS id, date_format(created_at,'%d-%m-%Y') AS fecha FROM journal WHERE is_closed != 1;"
+    "SELECT id_journal AS id, date_format(created_at,'%d-%m-%Y') AS fecha FROM journal WHERE is_closed = 0"
   );
   const workers = await db.query(
-    "SELECT id_worker AS id, CONCAT(name,' ',last_name ) AS fullname FROM workers ORDER BY fullname;"
+    "SELECT id_worker AS id, CONCAT(name,' ',last_name ) AS fullname FROM workers ORDER BY fullname"
   );
   res.render("assistances/newentrance", { journals, workers });
 });
